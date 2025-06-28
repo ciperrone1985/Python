3807aab8348c8ce67276330f89d6533f03c64047
ciperrone1985 <48599387+ciperrone1985@users.noreply.github.com>
2025-06-28 15:39:38 -0300

    Update pi_cycle_top_certo.py
    Inclusão do try e finally

--- pi_cycle_top_certo.py
+++ pi_cycle_top_certo.py
@@ -28,14 +28,24 @@ options.add_argument("--disable-blink-features=AutomationControlled")
 
 #  Ele burla o campo de verificação e apresenta o site
 driver = webdriver.Chrome(service=caminho_servico, options=options)
-driver.get("https://charts.bitbo.io/pi-cycle-top/")
 
-# Aguarda alguns segundos para garantir que o gráfico foi carregado
-import time
-# aguarda 60 segundos 
-time.sleep(60)
+#Try e finally finaliza a sessão caso dê algum problema na execução do script
+try: 
+    driver.get("https://charts.bitbo.io/pi-cycle-top/")
+    
+    # Aguarda alguns segundos para garantir que o gráfico foi carregado
+    import time
+    # aguarda 60 segundos 
+    time.sleep(60)
+    
+    
+    # Ajusta o print para pegar a tela toda
+    altura = driver.execute_script("return document.body.parentNode.scrollHeight")
+    largura = driver.execute_script("return document.body.parentNode.scrollWidth")
+    driver.set_window_size(largura, altura)
+    
+    # Salva a imagem no diretório onde o script está salvo
+    driver.save_screenshot("pi_cycle_top.png")
 
-# Salva a imagem no diretório onde o script está salvo
-driver.save_screenshot("pi_cycle_top.png")
-
-driver.quit()
+finally:  
+    driver.quit()
