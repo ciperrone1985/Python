28635baec671ec31bf4c10bb7efbd9305b172216
ciperrone1985 <48599387+ciperrone1985@users.noreply.github.com>
2025-06-28 15:20:56 -0300

    Update pi_cycle_top_certo.py
    Alteração do script para adicionar a imagem no diretório onde está o script

--- pi_cycle_top_certo.py
+++ pi_cycle_top_certo.py
@@ -17,18 +17,25 @@ driver.get("https://charts.bitbo.io/pi-cycle-top/")
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 
-# Caminho onde está instalado chrome
-caminho_servico = Service(r"C:\\Users\\perro\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe")
-
+# Caminho onde está instalado o executável chrome
+caminho_servico = Service(
+    r"C:\Users\perro\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe"
+)
 # injetar argumentos para desabilitar comando
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
 
-driver = webdriver.Chrome(service=caminho_servico, options=options)
-
 #  Ele burla o campo de verificação e apresenta o site
+driver = webdriver.Chrome(service=caminho_servico, options=options)
 driver.get("https://charts.bitbo.io/pi-cycle-top/")
+
+# Aguarda alguns segundos para garantir que o gráfico foi carregado
+import time
+# aguarda 60 segundos 
+time.sleep(60)
+
+# Salva a imagem no diretório onde o script está salvo
+driver.save_screenshot("pi_cycle_top.png")
+
+driver.quit()
